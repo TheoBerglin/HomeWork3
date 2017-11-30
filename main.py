c9040d0ad58db4d72295c0ffab195030af856f18
Theo Berglin <theoberglin@outlook.com>
2017-11-30 10:20:42 +0100

    Small patch

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from Small_world import generate_small_world_problem
 
 
 def run_task_1():
-    n = 400
+    n = 40
     p = 0.1
     print("Running Erdos-Renyi random graph")
     print("%d nodes with connection probability %1.2f" % (n, p))
@@ -26,4 +26,4 @@ def main(task_nbr):
         run_task_2()
 
 if __name__ == '__main__':
-    main(2)
+    main(1)
